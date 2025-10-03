901a445c0dd0026a2b0bb2e471f18c56a08c7752
didikh <com.didikhariyanto@gmail.com>
2025-10-03 14:24:11 +0700

    clean file

--- update-password.sql
+++
@@ -1,3 +0,0 @@
--- Test multiple known BCrypt hashes for "admin123"
-UPDATE employees SET password = '$2a$10$N.zmdr9k7uOCQb376NoUnuTJ8iAt6Z5EHsM8lE9b2.2L.4f9i8H8u' WHERE email = 'employee@hris.com';
--- This is the original hash from V6 migration
